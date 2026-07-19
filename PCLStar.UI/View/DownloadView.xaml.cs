c2fa932746570c9c328becd33fdab9ee8f3ad8e5
LaterChip <bfgh12345@outlook.com>
2026-07-19 10:26:23 +0800

    Update DownloadView.xaml.cs

--- PCLStar.UI/View/DownloadView.xaml.cs
+++ PCLStar.UI/View/DownloadView.xaml.cs
@@ -1,1 +1,12 @@
+using System.Windows.Controls;
 
+namespace PCLStar.UI.Views
+{
+    public partial class DownloadView : UserControl
+    {
+        public DownloadView()
+        {
+            InitializeComponent();
+        }
+    }
+}
