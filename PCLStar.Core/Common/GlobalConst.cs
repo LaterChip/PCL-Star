93af21eefa0c373e1f53c16d081915fdc709fc29
LaterChip <bfgh12345@outlook.com>
2026-07-19 10:18:34 +0800

    Refactor GlobalConfig to GlobalConst and simplify structure
    Refactor GlobalConfig class into GlobalConst static class and remove JSON serialization methods.

--- PCLStar.Core/Common/GlobalConst.cs
+++ PCLStar.Core/Common/GlobalConst.cs
@@ -1,23 +1,9 @@
 using System.IO;
-using Newtonsoft.Json;
 
-namespace PCLStar.Core.Config
+namespace PCLStar.Core
 {
-    public class GlobalConfig
+    public static class GlobalConst
     {
-        public string MinecraftDir { get; set; } = @".\.minecraft";
-        public string JavaPath { get; set; } = "";
-        public int MaxMemory { get; set; } = 2048;
-        public string SelectedTheme { get; set; } = "Dark";
-
-        private static string ConfigPath => Path.Combine(GlobalConst.DataDir, "global.json");
-
-        public void Save() => File.WriteAllText(ConfigPath, JsonConvert.SerializeObject(this, Formatting.Indented));
-
-        public static GlobalConfig Load()
-        {
-            if (!File.Exists(ConfigPath)) return new GlobalConfig();
-            return JsonConvert.DeserializeObject<GlobalConfig>(File.ReadAllText(ConfigPath));
-        }
+        public static string DataDir = Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.LocalApplicationData), "PCLStar");
     }
 }
