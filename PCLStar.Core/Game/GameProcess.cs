24a26526748f6d69d63ba4360fcaa0f07a8da219
LaterChip <bfgh12345@outlook.com>
2026-07-19 10:15:23 +0800

    Update GameProcess.cs

--- PCLStar.Core/Game/GameProcess.cs
+++ PCLStar.Core/Game/GameProcess.cs
@@ -1,1 +1,32 @@
+using System;
+using System.Diagnostics;
+using System.Threading.Tasks;
 
+namespace PCLStar.Core.Game
+{
+    public class GameProcess
+    {
+        public Process Process { get; private set; }
+
+        public async Task StartAsync(LaunchArgument args)
+        {
+            var startInfo = new ProcessStartInfo
+            {
+                FileName = args.JavaPath,
+                Arguments = string.Join(" ", args.BuildJvmArgs()) + " " + string.Join(" ", args.BuildGameArgs()),
+                UseShellExecute = false,
+                RedirectStandardOutput = true,
+                RedirectStandardError = true,
+                WorkingDirectory = args.GameDir
+            };
+            Process = Process.Start(startInfo);
+            await Task.CompletedTask;
+        }
+
+        public async Task<int> WaitForExitAsync()
+        {
+            await Process.WaitForExitAsync();
+            return Process.ExitCode;
+        }
+    }
+}
