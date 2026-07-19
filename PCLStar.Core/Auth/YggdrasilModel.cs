cfc189c62f2c71c63196f7ab6f5117b6b6cb050b
LaterChip <bfgh12345@outlook.com>
2026-07-19 10:10:58 +0800

    Update YggdrasilModel.cs

--- PCLStar.Core/Auth/YggdrasilModel.cs
+++ PCLStar.Core/Auth/YggdrasilModel.cs
@@ -1,1 +1,24 @@
+using Newtonsoft.Json;
 
+namespace PCLStar.Core.Auth
+{
+    public class YggdrasilAuthResponse
+    {
+        [JsonProperty("accessToken")] public string AccessToken { get; set; }
+        [JsonProperty("clientToken")] public string ClientToken { get; set; }
+        [JsonProperty("selectedProfile")] public Profile SelectedProfile { get; set; }
+        [JsonProperty("user")] public User User { get; set; }
+    }
+
+    public class Profile
+    {
+        [JsonProperty("id")] public string Id { get; set; }
+        [JsonProperty("name")] public string Name { get; set; }
+    }
+
+    public class User
+    {
+        [JsonProperty("id")] public string Id { get; set; }
+        [JsonProperty("username")] public string Username { get; set; }
+    }
+}
