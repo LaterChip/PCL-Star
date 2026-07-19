349f33fed2cb7c0f3e56090eb627849fcef050ad
LaterChip <bfgh12345@outlook.com>
2026-07-19 10:16:54 +0800

    Update ModManager.cs

--- PCLStar.Core/Mod/ModManager.cs
+++ PCLStar.Core/Mod/ModManager.cs
@@ -1,1 +1,32 @@
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
 
+namespace PCLStar.Core.Mod
+{
+    public class ModManager
+    {
+        private readonly string _modDir;
+
+        public ModManager(string gameDir)
+        {
+            _modDir = Path.Combine(gameDir, "mods");
+            Directory.CreateDirectory(_modDir);
+        }
+
+        public List<string> GetModList() => Directory.GetFiles(_modDir, "*.jar").Select(Path.GetFileName).ToList();
+
+        public void InstallMod(string jarPath)
+        {
+            var fileName = Path.GetFileName(jarPath);
+            var dest = Path.Combine(_modDir, fileName);
+            File.Copy(jarPath, dest, true);
+        }
+
+        public void DeleteMod(string fileName)
+        {
+            var path = Path.Combine(_modDir, fileName);
+            if (File.Exists(path)) File.Delete(path);
+        }
+    }
+}
