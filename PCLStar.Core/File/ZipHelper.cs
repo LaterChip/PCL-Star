caff02ef7d6904d52924b861217fd2c955a1af41
LaterChip <bfgh12345@outlook.com>
2026-07-19 10:17:31 +0800

    Update ZipHelper.cs

--- PCLStar.Core/File/ZipHelper.cs
+++ PCLStar.Core/File/ZipHelper.cs
@@ -1,1 +1,18 @@
+using SharpCompress.Archives;
+using SharpCompress.Common;
 
+namespace PCLStar.Core.File
+{
+    public static class ZipHelper
+    {
+        public static void ExtractZip(string zipPath, string outDir)
+        {
+            using var archive = ArchiveFactory.Open(zipPath);
+            foreach (var entry in archive.Entries)
+            {
+                if (!entry.IsDirectory)
+                    entry.WriteToDirectory(outDir, new ExtractionOptions { ExtractFullPath = true, Overwrite = true });
+            }
+        }
+    }
+}
