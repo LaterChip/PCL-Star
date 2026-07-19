01910b506e02cb29fe84b8722ad6ab99d0b925cc
LaterChip <bfgh12345@outlook.com>
2026-07-19 10:11:45 +0800

    Update OfflineAuth.cs

--- PCLStar.Core/Auth/OfflineAuth.cs
+++ PCLStar.Core/Auth/OfflineAuth.cs
@@ -1,1 +1,19 @@
+using System.Threading.Tasks;
 
+namespace PCLStar.Core.Auth
+{
+    public class OfflineAuth
+    {
+        public async Task<YggdrasilAuthResponse> LoginAsync(string username)
+        {
+            await Task.CompletedTask;
+            return new YggdrasilAuthResponse
+            {
+                AccessToken = "offline",
+                ClientToken = "offline",
+                SelectedProfile = new Profile { Id = "offline", Name = username },
+                User = new User { Id = "offline", Username = username }
+            };
+        }
+    }
+}
