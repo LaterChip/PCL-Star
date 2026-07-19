3db4a5a076fc9e9f7841d783e06e5b7082a3f837
LaterChip <bfgh12345@outlook.com>
2026-07-19 10:12:02 +0800

    Update LittleskinAuth.cs

--- PCLStar.Core/Auth/LittleskinAuth.cs
+++ PCLStar.Core/Auth/LittleskinAuth.cs
@@ -1,1 +1,17 @@
+using System.Threading.Tasks;
 
+namespace PCLStar.Core.Auth
+{
+    public class LittleSkinAuth : YggdrasilApi
+    {
+        public LittleSkinAuth()
+        {
+            BaseUrl = "https://littleskin.cn/api/yggdrasil";
+        }
+        
+        public async Task<YggdrasilAuthResponse> LoginWithLittleSkinAsync(string email, string password)
+        {
+            return await AuthenticateAsync(email, password);
+        }
+    }
+}
