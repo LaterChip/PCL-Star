6ea0eadeac95bcd168ddeec075f050fa261d195b
LaterChip <bfgh12345@outlook.com>
2026-07-19 09:56:07 +0800

    Update Program.cs

--- PCLStar.Launcher/Program.cs
+++ PCLStar.Launcher/Program.cs
@@ -1,1 +1,16 @@
+using System;
+using System.Windows;
 
+namespace PCLStar.Launcher
+{
+    public class Program
+    {
+        [STAThread]
+        public static void Main()
+        {
+            var app = new App();
+            app.InitializeComponent();
+            app.Run();
+        }
+    }
+}
