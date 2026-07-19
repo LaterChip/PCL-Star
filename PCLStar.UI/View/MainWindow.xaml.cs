2d5a2ef25ae7b27f7134e13b62345cac1fced675
LaterChip <bfgh12345@outlook.com>
2026-07-19 10:20:17 +0800

    Update MainWindow.xaml.cs

--- PCLStar.UI/View/MainWindow.xaml.cs
+++ PCLStar.UI/View/MainWindow.xaml.cs
@@ -1,1 +1,17 @@
+using System.Windows;
+using PCLStar.UI.ViewModels;
 
+namespace PCLStar.UI.Views
+{
+    public partial class MainWindow : Window
+    {
+        public MainViewModel ViewModel { get; } = new MainViewModel();
+
+        public MainWindow()
+        {
+            InitializeComponent();
+            DataContext = ViewModel;
+            MainFrame.Navigate(new LoginWindow());
+        }
+    }
+}
