dd0b5e29e81355a9755b516edf98ebf784203df2
LaterChip <bfgh12345@outlook.com>
2026-07-19 10:12:20 +0800

    Update ExternalAuth.cs

--- PCLStar.Core/Auth/ExternalAuth.cs
+++ PCLStar.Core/Auth/ExternalAuth.cs
@@ -1,1 +1,10 @@
-
+namespace PCLStar.Core.Auth
+{
+    public class ExternalAuth : YggdrasilApi
+    {
+        public ExternalAuth(string customUrl)
+        {
+            BaseUrl = customUrl.TrimEnd('/');
+        }
+    }
+}
