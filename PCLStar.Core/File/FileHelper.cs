c100384fa62a7e7d46676327062104a7877a5780
LaterChip <bfgh12345@outlook.com>
2026-07-19 10:17:13 +0800

    Update FileHelper.cs

--- PCLStar.Core/File/FileHelper.cs
+++ PCLStar.Core/File/FileHelper.cs
@@ -1,1 +1,11 @@
+using System.IO;
 
+namespace PCLStar.Core.File
+{
+    public static class FileHelper
+    {
+        public static void EnsureDirectory(string path) => Directory.CreateDirectory(path);
+        public static string CombinePath(params string[] parts) => Path.Combine(parts);
+        public static long GetSize(string path) => File.Exists(path) ? new FileInfo(path).Length : 0;
+    }
+}
