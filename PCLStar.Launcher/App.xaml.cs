97812ea93713c27169b50baf97476d9eb8ede226
LaterChip <bfgh12345@outlook.com>
2026-07-19 09:56:56 +0800

    Update App.xaml.cs

--- PCLStar.Launcher/App.xaml.cs
+++ PCLStar.Launcher/App.xaml.cs
@@ -1,1 +1,18 @@
+using System.Windows;
 
+namespace PCLStar.Launcher
+{
+    public partial class App : Application
+    {
+        protected override void OnStartup(StartupEventArgs e)
+        {
+            base.OnStartup(e);
+            // 全局异常捕获等
+            Current.DispatcherUnhandledException += (s, args) =>
+            {
+                MessageBox.Show($"发生错误: {args.Exception.Message}", "PCLStar");
+                args.Handled = true;
+            };
+        }
+    }
+}
