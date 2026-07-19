81ec533af23b452620b06373cdac3384c32f275a
LaterChip <bfgh12345@outlook.com>
2026-07-19 10:18:10 +0800

    Update LogHelper.cs

--- PCLStar.Core/Common/LogHelper.cs
+++ PCLStar.Core/Common/LogHelper.cs
@@ -1,1 +1,20 @@
+using System;
+using System.IO;
 
+namespace PCLStar.Core.Common
+{
+    public static class LogHelper
+    {
+        private static string _logPath => Path.Combine(GlobalConst.DataDir, "logs", $"{DateTime.Now:yyyy-MM-dd}.log");
+        static LogHelper() => Directory.CreateDirectory(Path.GetDirectoryName(_logPath));
+
+        public static void Log(string msg)
+        {
+            var line = $"[{DateTime.Now:HH:mm:ss}] {msg}";
+            Console.WriteLine(line);
+            File.AppendAllText(_logPath, line + Environment.NewLine);
+        }
+
+        public static void Error(Exception ex) => Log($"ERROR: {ex.Message}\n{ex.StackTrace}");
+    }
+}
